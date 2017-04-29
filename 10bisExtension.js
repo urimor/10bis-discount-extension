b69d33245b35270c645f92418af47f424797af18
Uri Mor <uri.mor@gmail.com>
2017-04-29 10:50:28 +0300

    Add the new price after discount
    Read the discount from the page and add the new price after discount
    with a dedicated formatting

---
+++ 10bisExtension.js
@@ -0,0 +1,45 @@
+if(isThereADiscount()){
+	let discount = getDiscount();
+	if(typeof discount !== 'undefined') {
+		updatePrices(discount);
+	}
+}
+
+function isThereADiscount() {
+	let discountDivs = document.getElementsByClassName('discountCouponImgHeaderRed SpecialDiscountPercent');
+	if(typeof discountDivs === 'undefined'){
+		return false;
+	}
+	let theDiscountDiv = discountDivs[0];
+	if(typeof theDiscountDiv === 'undefined'){
+		return false;
+	}
+	let regExpMatch = theDiscountDiv.innerHTML.match(/(\d+)\%/);
+	if(regExpMatch === null || regExpMatch.length < 2){
+		return false;
+	}
+	return true;
+}
+
+function getDiscount() {
+	let discountDiv = document.getElementsByClassName('discountCouponImgHeaderRed SpecialDiscountPercent')[0];
+	if(discountDiv){
+		let discount = discountDiv.innerHTML.match(/(\d+)\%/)[1];
+		return parseInt(discount)/100;
+	}
+}
+
+function updatePrices(discount) {
+	let priceDivs = document.getElementsByClassName('dishPriceDiv');
+	for(let priceDiv of priceDivs) {
+		priceDiv.innerHTML = priceDiv.innerHTML.replace(/₪ (\d+.\d+)/,(match, price) => {
+			let newPrice = getNewPrice(price,discount).toFixed(2);
+			let newPriceString = match.replace(/\d+.\d+/,newPrice);
+			return `<span class='new-price'>${newPriceString}</span><span class='old-price'>${match}</span>`;
+		});
+	};
+}
+
+function getNewPrice(oldPrice, discount) {
+	return parseFloat(oldPrice) * (1 - discount);
+}
