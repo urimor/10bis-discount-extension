01c202dcec1f51148d38a7b30aee8cf057607c0e
Uri Mor <uri.mor@gmail.com>
2017-05-03 22:06:53 +0300

    Add page action title
    Add default title and change the title to display the discount in case
    there is a discount

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
 	if(message.type === '10bisDiscountShowIcon'){
 		chrome.pageAction.show(sender.tab.id);
+		chrome.pageAction.setTitle({
+								tabId: sender.tab.id,
+								title: `10bis: ${message.discount * 100}% off`
+		});
 	}
 });
 
