6446e0b514c2b101bf59d7632efd74ea9d8145d5
Uri Mor <uri.mor@gmail.com>
2017-05-07 22:45:24 +0300

    Formatting
    Format code and change let to const where possible

--- 10bisExtension.js
+++ 10bisExtension.js
@@ -1,30 +1,30 @@
-let discount = getDiscount();
-if(discount > 0) {
+const discount = getDiscount();
+if (discount > 0) {
 	chrome.runtime.sendMessage({
-		type:'10bisDiscountShowIcon',
+		type: '10bisDiscountShowIcon',
 		discount: discount
 	});
 	updatePrices(discount);
 }
 
 function getDiscount() {
-	let discountDiv = document.getElementsByClassName('discountCouponImgHeaderRed SpecialDiscountPercent')[0];
-	if(discountDiv){
+	const discountDiv = document.getElementsByClassName('discountCouponImgHeaderRed SpecialDiscountPercent')[0];
+	if (discountDiv) {
 		let discount = discountDiv.innerHTML.match(/(\d+)\%/)[1];
-		return parseInt(discount)/100;
+		return parseInt(discount) / 100;
 	}
 	return 0;
 }
 
 function updatePrices(discount) {
-	let priceDivs = document.getElementsByClassName('dishPriceDiv');
-	for(let priceDiv of priceDivs) {
-		priceDiv.innerHTML = priceDiv.innerHTML.replace(/₪ (\d+.\d+)/,(match, price) => {
-			let newPrice = getNewPrice(price,discount).toFixed(2);
-			let newPriceString = match.replace(/\d+.\d+/,newPrice);
+	const priceDivs = document.getElementsByClassName('dishPriceDiv');
+	for (let priceDiv of priceDivs) {
+		priceDiv.innerHTML = priceDiv.innerHTML.replace(/₪ (\d+.\d+)/, (match, price) => {
+			const newPrice = getNewPrice(price, discount).toFixed(2);
+			const newPriceString = match.replace(/\d+.\d+/, newPrice);
 			return `<span class='new-price'>${newPriceString}</span> <span class='old-price'>${match}</span>`;
 		});
-	};
+	}
 }
 
 function getNewPrice(oldPrice, discount) {
